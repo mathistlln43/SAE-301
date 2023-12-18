4394529348c26e64257189b612780919e471676c
MelodieSerre <153275023+MelodieSerre@users.noreply.github.com>
2023-12-18 15:22:48 +0100

    Footer
    Footer en php + la partie CSS + Images pour le Footer

---
+++ footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+    <footer>
+        <!-- <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <img src="Images/patrick.png">
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div>
+                            <img src="Images/mail.png" alt="mail">
+                        </div>
+                        <div>
+                            <p>06 78 08 94 77</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <p>amisdelamusique63@gmail.com</p>
+        </div>-->
+
+       <div id="logo">
+            <img src="Images/logo_Les-amis-de-la-musique_blanc.svg" alt="Logo">
+        </div> 
+
+        <div class="colonne">
+            <div class="Contact">
+                <img src="Images/mail.png" alt="mail">
+                <p>06 78 08 94 77</p>
+            </div>
+            <div class="mail">
+                <img src="Images/telephone.png" alt="">
+                <a href="mailto:amisdelamusique63@gmail.com">
+                    <p>amisdelamusique63@gmail.com</p>
+                </a>
+            </div>
+        </div>
+        <div class="reseaux">
+            <img src="Images/facebook.png" alt="">
+        </div>
+
+    </footer>
+</body>
+
+</html>
