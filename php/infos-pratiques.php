782e93dcaf08814de5a74f48a7cdf9c768154e20
luciemz <luciemanzoni@free.fr>
2023-12-19 18:36:32 +0100

    Début Infos pratiques

---
+++ php/infos-pratiques.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Informations pratiques</title>
+
+        <link href="../css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <header>
+           <img src="../images/piano.jpg">
+           <div id="trapeze">
+               <div class="trapezeContenu">
+                    <p id="event">Informations pratiques</p>
+               </div>
+           </div>
+        </header>
+
+        <section id="infosPratiques">
+            <div id="IFbillets">
+                <img class="hautRect" src="..\images\theatre.jpg" alt="">
+                <img class="basRect" src="..\images\theatre.jpg" alt="">
+                <div id="blocBillet">
+                    <h1 class="titreIF">Billetterie en ligne</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <button class="boutonIF">Billetterie</button>
+            </div>
+        </section>
+
+        <footer>
+            <img id="logo" src="../images/mairie.jpg">
+
+            <div id="footerContact">
+                <div class="contact">
+                    <img src="../images/phone.svg">
+                    <p>+33 6 78 08 94 77</p>
+                </div>
+                <div class="contact">
+                    <img src="../images/mail.svg">
+                    <p>amisdelamusique63@gmail.com</p>
+                </div>
+            </div>
+
+            <img class="reseauxPart" src="../images/facebook_blanc.png">
+        </footer>
+        
+    </body>
+
+</html>
