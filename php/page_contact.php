df188205f8b094480d0fe343473d73405f0c3eaf
MelodieSerre <153275023+MelodieSerre@users.noreply.github.com>
2023-12-19 16:12:35 +0100

    Page Contact.php
    Ajout de la page page_contact.php

---
+++ php/page_contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HEADER</title>
+  <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+<div class="Contact">
+  <h1>Contact</h1>
+</div>
+</body>
+
+</html>
+
+<style>
+  .Contact{
+text-align: right;
+  }
+  .Contact h1{
+    color: rrr;
+  }
+</style>
