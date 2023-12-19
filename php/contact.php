892d8588f8bad1b3cd59e451332393a53c9f0b01
luciemz <luciemanzoni@free.fr>
2023-12-19 16:12:27 +0100

    MAJ contact + CSS (maintenant responsive, manque img, icones, menu et footer)

--- php/contact.php
+++ php/contact.php
@@ -14,7 +14,12 @@
            <div id="trapeze2">
                <div class="trapeze2Contenu">
                     <p id="contact">Contact</p>
-                    <p></p>
+                    <div>
+                        <p>+33 6 78 08 94 77</p>
+                    </div>
+                    <div>
+                        <p>amisdelamusique63@gmail.com</p>
+                    </div>
                </div>
            </div>
         </header>
@@ -23,24 +28,24 @@
         <form action="" method="post" id="formContact">
             <p>Une question ?</p>
             <div class="ligne">
-                <input type="text" classe="demi-ligne" placeholder="Nom *"/>
-                <input type="text" classe="demi-ligne" placeholder="Prénom *"/>
+                <input type="text" classe="demi-ligne" placeholder="Nom *" required/>
+                <input type="text" classe="demi-ligne" placeholder="Prénom *" required/>
             </div>
             
             <div class="ligne">
-                <input type="email" placeholder="Email *"/>
-                <input type="tel" placeholder="Téléphone *"/>
+                <input type="email" placeholder="Email *" required/>
+                <input type="tel" placeholder="Téléphone"/>
             </div>
 
-            <input type="text" id="objet" placeholder="Objet de votre demande *"/>
-            <input type="textarea" id="message" placeholder="Message... *"/>
+            <input type="text" id="objet" placeholder="Objet de votre demande *" required/>
+            <textarea id="message" placeholder="Message... *" rows="5" required></textarea>
 
             <div>
                 <input type="checkbox"/>
                 <label>En soumettant ce formulaire, j’accepte que les informations saisies soient exploitées par les Amis de la Musique *</label>
             </div>
             
-            <input class="envoiContact" type="submit" value="ENVOYER" />
+            <input id="envoiContact" type="submit" value="Envoyer" />
             
         </form>
         
