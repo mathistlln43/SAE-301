6851859e694ae02ff7687cf53cce0f39254dce96
J3rm4 <136993661+J3rm4@users.noreply.github.com>
2023-12-20 09:09:43 +0100

    Update suppression_artiste.php

--- php/ExecAdmin/suppression_artiste.php
+++ php/ExecAdmin/suppression_artiste.php
@@ -1,5 +1,8 @@
 <?php
-include("../config.php");
+
+require('../config.php');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Vérifier la connexion
 if ($conn->connect_error) {
@@ -7,16 +10,16 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer l'identifiant de l'artiste à supprimer depuis le formulaire
     $id_artiste = $_POST['id_artiste'];
 
-    $sql = "DELETE FROM artistes WHERE id = $id_artiste"; 
+    $sql = "DELETE FROM artiste WHERE id_artiste = $id_artiste";
     if ($conn->query($sql) === TRUE) {
         echo "Artiste supprimé avec succès.";
     } else {
         echo "Erreur lors de la suppression de l'artiste : " . $conn->error;
     }
 }
-
 
 $conn->close();
 ?>
