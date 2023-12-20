c946967039a51e571021648bd603c9f7a710d648
MelodieSerre <153275023+MelodieSerre@users.noreply.github.com>
2023-12-20 19:58:49 +0100

    rectification des liens pour les images dans le footer
    rectification des liens pour les images

--- php/footer.php
+++ php/footer.php
@@ -10,45 +10,24 @@
 
 <body>
     <footer>
-        <!-- <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-md-6 col-lg-4">
-                    <img src="Images/patrick.png">
-                </div>
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div>
-                            <img src="Images/mail.png" alt="mail">
-                        </div>
-                        <div>
-                            <p>06 78 08 94 77</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-        <p>amisdelamusique63@gmail.com</p>
-        </div>-->
-
        <div id="logo">
             <img src="images/Logo Blanc.png" alt="Logo">
         </div> 
 
         <div class="colonne">
             <div class="Contact">
-                <img src="Images/mail.png" alt="mail">
+                <img src="images/mail.png" alt="mail">
                 <p>06 78 08 94 77</p>
             </div>
             <div class="mail">
-                <img src="Images/telephone.png" alt="">
+                <img src="images/telephone.png" alt="">
                 <a href="mailto:amisdelamusique63@gmail.com">
                     <p>amisdelamusique63@gmail.com</p>
                 </a>
             </div>
         </div>
         <div class="reseaux">
-            <img src="Images/facebook.png" alt="">
+            <img src="images/facebook.png" alt="">
         </div>
 
     </footer>
