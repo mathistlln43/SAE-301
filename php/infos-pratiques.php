0523e44074905ff0f566d9eff6d8be7dd268c97a
luciemz <luciemanzoni@free.fr>
2023-12-20 18:47:09 +0100

    Suite Infos pratiques (1ere et 2e zones + css)

--- php/infos-pratiques.php
+++ php/infos-pratiques.php
@@ -20,6 +20,34 @@
         </header>
 
         <section id="infosPratiques">
+
+            <article class="infosP">
+                <img class="imgFondIF" id="fondIF1" src="..\images\theatre.jpg">
+                <div class="cartesIF">
+                    <div id="reglesMusiciens">
+                        <h1>Règles pour les musiciens</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <img src="..\images\theatre.jpg">
+                    <button class="boutonIF" id="btnIF1">Artistes</button>
+                </div>
+            </article>
+
+            <article class="infosP">
+                <img class="imgFondIF" id="fondIF2" src="..\images\theatre.jpg">
+                <div class="cartesIF">
+                    <img src="..\images\theatre.jpg">
+                    <div id="LieuDate">
+                        <h1>Règles pour les musiciens</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <button class="boutonIF" id="btnIF2">Artistes</button>
+                    </div>
+                </div>
+            </article>            
+
+        </section>
+
+        <!--<section id="infosPratiques">
             <div id="IFbillets">
                 <img class="hautRect" src="..\images\theatre.jpg" alt="">
                 <img class="basRect" src="..\images\theatre.jpg" alt="">
@@ -29,24 +57,7 @@
                 </div>
                 <button class="boutonIF">Billetterie</button>
             </div>
-        </section>
-
-        <footer>
-            <img id="logo" src="../images/mairie.jpg">
-
-            <div id="footerContact">
-                <div class="contact">
-                    <img src="../images/phone.svg">
-                    <p>+33 6 78 08 94 77</p>
-                </div>
-                <div class="contact">
-                    <img src="../images/mail.svg">
-                    <p>amisdelamusique63@gmail.com</p>
-                </div>
-            </div>
-
-            <img class="reseauxPart" src="../images/facebook_blanc.png">
-        </footer>
+        </section>-->
         
     </body>
 
